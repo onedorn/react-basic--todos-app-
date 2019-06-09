e944fa681e019902827ef4b2200e3facab3ea69b
Volodymyr Syvoraksha <vpsyvoraksha@gmail.com>
2019-06-09 19:12:46 +0300

    Todos component created and rendering my jsx template to the DOM. EventListener deleting todos from the list so far.

--- todos-app/src/App.js
+++ todos-app/src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import Todos from './Todos'
 
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Code goes here</h1>
-    </div>
-  );
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            todos: [
+                {id: 1, content: 'buy some milk bitch'},
+                {id: 2, content: 'play guitar and more'},
+            ]
+        }
+    }
+    deleteTodo = (id) => {
+        const todos = this.state.todos.filter(todo => {
+            return todo.id !==id;
+        })
+        this.setState({
+            todos
+        })
+    }
+    render() {
+        return (
+            <div className="todo-app container">
+                <h1 className="center blue-text">Todos</h1>
+                <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+            </div>
+        );
+    }
 }
 
 export default App;
